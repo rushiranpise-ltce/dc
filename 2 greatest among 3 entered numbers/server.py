1f516696fc844e8e1fb180575d90f0bb895fedd6
rushiranpise-ltce <109956819+rushiranpise-ltce@users.noreply.github.com>
2023-04-25 23:22:49 +0530

    Update server.py

--- 2 greatest among 3 entered numbers/server.py
+++ 2 greatest among 3 entered numbers/server.py
@@ -1,13 +1,15 @@
 import xmlrpc.server
 
-# Define the function to find the greatest number
+# function to find the greatest number
 def find_greatest(num1, num2, num3):
     return max(num1, num2, num3)
 
-# Create an XML-RPC server and register the function
+# create a server object
 server = xmlrpc.server.SimpleXMLRPCServer(('localhost', 8000))
+
+# register the function with the server
 server.register_function(find_greatest, 'find_greatest')
 
-# Start the server
+# start the server and listen for incoming requests
 print('Server listening on port 8000...')
 server.serve_forever()
