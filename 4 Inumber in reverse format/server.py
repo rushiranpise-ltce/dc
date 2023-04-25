4ee7a7f115212e6a117ae6e853e9c43dc38fc55b
rushiranpise-ltce <109956819+rushiranpise-ltce@users.noreply.github.com>
2023-04-25 23:27:46 +0530

    Update server.py

--- 4 Inumber in reverse format/server.py
+++ 4 Inumber in reverse format/server.py
@@ -1,5 +1,6 @@
 import xmlrpc.server
 
+# Define a function to reverse a number
 def rev(num):
     rev_num = 0
     while num!=0:
@@ -8,10 +9,12 @@ def rev(num):
         num //= 10
     return rev_num
 
-# Create an XML-RPC server and register the function
+# Create an XML-RPC server instance
 server = xmlrpc.server.SimpleXMLRPCServer(('localhost', 8000))
+
+# Register the function with the server, and give it a name
 server.register_function(rev, 'rev')
 
-# Start the server
+# Start the server and print a message
 print('Server listening on port 8000...')
 server.serve_forever()
