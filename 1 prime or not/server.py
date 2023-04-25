f22757aa74bb39dab4c963d6fcee3ac8ac706804
rushiranpise-ltce <109956819+rushiranpise-ltce@users.noreply.github.com>
2023-04-25 23:01:22 +0530

    Update server.py

--- 1 prime or not/server.py
+++ 1 prime or not/server.py
@@ -1,6 +1,7 @@
+# Importing the xmlrpc server library
 import xmlrpc.server
 
-# function to check if a number is prime or not
+# Defining a function to check if a number is prime
 def is_prime(n):
     if n < 2:
         return False
@@ -9,12 +10,12 @@ def is_prime(n):
             return False
     return True
 
-# create an RPC server
+# Creating a SimpleXMLRPCServer object at "localhost" and port 8000
 server = xmlrpc.server.SimpleXMLRPCServer(("localhost", 8000))
 
-# register the is_prime function with the server
+# Registering the 'is_prime' function with the server and exposing it with the name 'is_prime'
 server.register_function(is_prime, "is_prime")
 
-# start the server
+# Starting the server and printing a message to indicate that it is listening on port 8000
 print('Server listening on port 8000...')
 server.serve_forever()
