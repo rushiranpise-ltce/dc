cf26e5d8b15239b64349a92fb4c757b494410263
rushiranpise-ltce <109956819+rushiranpise-ltce@users.noreply.github.com>
2023-04-25 23:13:01 +0530

    Update app.py

--- 14 Name Resolution protocol/app.py
+++ 14 Name Resolution protocol/app.py
@@ -1,13 +1,20 @@
+# Importing required modules
 import socket
+
+# Defining function to get IP address of a URL
 def get_ip_address(url):
-  try:
-    host_name = socket.gethostbyname(url)
-    host_ip = socket.gethostbyname(host_name)
-    print("Hostname :",host_name)
-    print("IP :",host_ip)
-  except:
-    print("Unable to get hostname and IP")
+    try:
+        # Getting the IP address of the given URL
+        host_name = socket.gethostbyname(url)
+        host_ip = socket.gethostbyname(host_name)
+        # Printing the host name and IP address
+        print("Hostname :", host_name)
+        print("IP :", host_ip)
+    except:
+        # Handling the exception if IP address is not found
+        print("Unable to get hostname and IP")
 # Driver code
-if __name__ == "__main__" :
-  url = "www.twitter.com"
-  get_ip_address(url)
+if __name__ == "__main__":
+    # Testing the function with a sample URL
+    url = "www.twitter.com"
+    get_ip_address(url)
