226714813d44b9d6f9eba38a3ee4e4f2e40c6a6d
rushiranpise-ltce <109956819+rushiranpise-ltce@users.noreply.github.com>
2023-04-25 23:23:52 +0530

    Update client.py

--- 3 fibbonn acci/client.py
+++ 3 fibbonn acci/client.py
@@ -1,8 +1,16 @@
+# This program demonstrates how to use an XML-RPC client to call a function on an XML-RPC server
+
 import xmlrpc.client
 
-# Create an XML-RPC server proxy
+
+# create an instance of the server proxy by passing the URL of the server
 proxy = xmlrpc.client.ServerProxy('http://localhost:8000/')
 
+# ask user to enter a value for n
 n = int(input("enter value of n: "))
+
+# call the "fib" function on the server and store the result in "result"
 result = proxy.fib(n)
-print(f"The fibonnacci series upto n is: {result}")
+
+# display the result
+print(f"The fibonacci series upto n is: {result}")
