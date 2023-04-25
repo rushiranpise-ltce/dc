92f7894d1ce7bdc0da0fff8a9e09f01489b7a177
rushiranpise-ltce <109956819+rushiranpise-ltce@users.noreply.github.com>
2023-04-25 23:26:19 +0530

    Update client.py

--- 4 Inumber in reverse format/client.py
+++ 4 Inumber in reverse format/client.py
@@ -1,8 +1,13 @@
 import xmlrpc.client
 
-# Create an XML-RPC server proxy
+# create a proxy object to access the remote XML-RPC server
 proxy = xmlrpc.client.ServerProxy('http://localhost:8000/')
 
+# get input from the user
 n = int(input("enter number: "))
+
+# call the remote method 'rev' with the input number as parameter
 result = proxy.rev(n)
+
+# display the result
 print(f"The reversed number is: {result}")
